d887364bee9abd828cb8b37333ca97c60187aa2c
T2Russ <110161637+T2Russ@users.noreply.github.com>
2025-07-23 16:08:44 +0200

    Update getProperties.js

--- BED-FINAL-PROJECT-BOILERPLATE/src/services/properties/getProperties.js
+++ BED-FINAL-PROJECT-BOILERPLATE/src/services/properties/getProperties.js
@@ -1,9 +1,19 @@
 import { PrismaClient } from "@prisma/client";
+ 
+const prisma = new PrismaClient();
 
-const getProperties = async () => {
-  const prisma = new PrismaClient();
-  const properties = await prisma.property.findMany();
+const getProperties = async (filters) => {
+  const where = {};
 
+  if (filters.location) {
+    where.location = { contains: filters.location, mode: "insensitive" };
+  }
+
+  if (filters.pricePerNight) {
+    where.pricePerNight = parseFloat(filters.pricePerNight);
+  }
+
+  const properties = await prisma.property.findMany({ where });
   return properties;
 };
 
