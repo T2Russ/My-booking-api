34f07a1a5044f93e7a0813db9ab6a1e0acaf75ed
T2Russ <110161637+T2Russ@users.noreply.github.com>
2025-07-23 16:58:30 +0200

    Update getReviews.js

--- BED-FINAL-PROJECT-BOILERPLATE/src/services/reviews/getReviews.js
+++ BED-FINAL-PROJECT-BOILERPLATE/src/services/reviews/getReviews.js
@@ -1,9 +1,18 @@
 import { PrismaClient } from "@prisma/client";
+const prisma = new PrismaClient();
 
-const getReviews = async () => {
-  const prisma = new PrismaClient();
-  const reviews = await prisma.review.findMany();
+const getReviews = async (filters = {}) => {
+  const where = {};
 
+  if (filters.propertyId) {
+    where.propertyId = filters.propertyId;
+  }
+
+  if (filters.userId) {
+    where.userId = filters.userId;
+  }
+
+  const reviews = await prisma.review.findMany({ where });
   return reviews;
 };
 
