466b1f694b82e6f689b2a0dec4db8a2382aca91e
T2Russ <110161637+T2Russ@users.noreply.github.com>
2025-07-31 11:24:01 +0200

    Update errorHandler.js

--- BED-FINAL-PROJECT-BOILERPLATE/src/middleware/errorHandler.js
+++ BED-FINAL-PROJECT-BOILERPLATE/src/middleware/errorHandler.js
@@ -1,14 +1,9 @@
 const errorHandler = (err, req, res, next) => {
-    console.error(err);
-    res
-      .status(500)
-      .json({
-        message:
-          "Error 500: An error occurred on the server, please check your request!",
-      });
-  
-    // next(err); 
-  };
-  
-  export default errorHandler;
-  
+  console.error(err);
+
+  res.status(err.status || err.statusCode || 500).json({
+    message: err.message || "Internal Server Error",
+  });
+};
+
+export default errorHandler;
